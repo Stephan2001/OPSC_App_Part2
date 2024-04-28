552859dfedad1386c8ffa1c6c16bf27812bca812
ST10219213 <moolmans20013@gmail.com>
2024-04-28 17:14:17 +0200

    authentication
    Added user authentication

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/MainActivity.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.timeflow_opsc_poe_part_2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            //load main pages
+        }
+        else{
+            val intent = Intent(this, Login_Page::class.java)
+            startActivity(intent)
         }
     }
 }
