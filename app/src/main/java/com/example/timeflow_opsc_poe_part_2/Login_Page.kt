7c950d96aa2f9cef656560fa37d25e2f85a5a011
Clene <104350902+Clene1@users.noreply.github.com>
2024-04-29 15:38:40 +0200

    changes added

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/Login_Page.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/Login_Page.kt
@@ -2,14 +2,22 @@ package com.example.timeflow_opsc_poe_part_2
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class Login_Page : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login_page)
@@ -26,7 +34,75 @@ class Login_Page : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val btnLogin = findViewById<Button>(R.id.btnSignIn)
+        btnLogin.setOnClickListener ()
+        {
+            val email: EditText = findViewById(R.id.txtEmail)
+            val pass: EditText = findViewById(R.id.txtPassword)
+            signIn(email.text.toString(), pass.text.toString())
+        }
+
+        val btnReg = findViewById<Button>(R.id.btnRegister)
+        btnReg.setOnClickListener ()
+        {
+            val email: EditText = findViewById(R.id.txtEmail)
+            val pass: EditText = findViewById(R.id.txtPassword)
+            createAccount(email.text.toString(), pass.text.toString())
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    //updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
 
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    //load pages with navigation
+                    startActivity(intent)
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+        // [END sign_in_with_email]
+    }
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
 }
