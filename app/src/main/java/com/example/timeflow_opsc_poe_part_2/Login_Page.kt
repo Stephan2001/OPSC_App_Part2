335008eb3bd0c900138dcff3abd622cede88b50f
Clene <104350902+Clene1@users.noreply.github.com>
2024-04-29 13:16:12 +0200

    Register page added

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/Login_Page.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/Login_Page.kt
@@ -1,6 +1,8 @@
 package com.example.timeflow_opsc_poe_part_2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,15 @@ class Login_Page : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var buttonReg : Button = findViewById(R.id.btnRegister)
+        buttonReg.setOnClickListener ()
+        {
+            val intent = Intent(this, Registration_page::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
 }
