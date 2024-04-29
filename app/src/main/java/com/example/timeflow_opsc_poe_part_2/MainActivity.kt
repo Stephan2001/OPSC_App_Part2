7751a0f5a61c05b1cb89b56e42e448bdc7210a09
Clene <104350902+Clene1@users.noreply.github.com>
2024-04-29 21:26:52 +0200

    Fragments added

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/MainActivity.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/MainActivity.kt
@@ -1,33 +1,26 @@
 package com.example.timeflow_opsc_poe_part_2
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Parcelable
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import com.google.firebase.auth.FirebaseAuth
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
@@ -39,7 +32,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         if(savedInstanceState == null){
-            replaceFragment(HomeFragment())
+            replaceFragment(ScheduleFragment())
             navigationView.setCheckedItem(R.id.nav_schedule)
         }
 
@@ -47,8 +40,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.nav_schedule -> replaceFragment(HomeFragment())
-            R.id.nav_schedule -> Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+            R.id.nav_schedule -> replaceFragment(ScheduleFragment())
+            R.id.nav_projects -> replaceFragment(ProjectsFragment())
+            R.id.nav_statistics -> replaceFragment(StatisticsFragment())
+            R.id.nav_goals -> replaceFragment(GoalsFragment())
+            R.id.nav_logout -> Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
