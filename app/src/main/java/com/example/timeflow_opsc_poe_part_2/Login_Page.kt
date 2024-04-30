daad3fefcdce6f268ef05187817416638452e2cb
ST10219213 <moolmans20013@gmail.com>
2024-04-30 15:14:59 +0200

    user logins fixed

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/Login_Page.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/Login_Page.kt
@@ -58,6 +58,7 @@ class Login_Page : AppCompatActivity() {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithEmail:success")
                     val user = auth.currentUser
+                    val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                 } else {
                     // If sign in fails, display a message to the user.
