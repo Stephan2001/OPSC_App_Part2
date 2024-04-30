8d6e7b8b7dc09dfb7ae29c492565da941fd59978
Clene <104350902+Clene1@users.noreply.github.com>
2024-04-30 14:02:25 +0200

    Select option activity added

---
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/Select_Option.kt
@@ -0,0 +1,20 @@
+package com.example.timeflow_opsc_poe_part_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Select_Option : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_select_option)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
