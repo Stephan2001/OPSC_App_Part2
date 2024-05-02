117b9a30c43db81570b0be6d50287c844bfd2483
ST10219213 <moolmans20013@gmail.com>
2024-05-02 11:27:42 +0200

    Dropdownlist data
    Added data to dropdown list for project priority

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/Project_Create.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/Project_Create.kt
@@ -1,6 +1,13 @@
 package com.example.timeflow_opsc_poe_part_2
 
 import android.os.Bundle
+import android.text.Selection.setSelection
+import android.view.Gravity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +18,8 @@ import com.google.firebase.database.FirebaseDatabase
 class Project_Create : AppCompatActivity() {
     private  lateinit var rootNode : FirebaseDatabase
     private  lateinit var projectReference : DatabaseReference
+    var priorities = arrayOf("High", "Low")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +31,28 @@ class Project_Create : AppCompatActivity() {
         }
         val currentUser = CurrentUser.userID
         rootNode = FirebaseDatabase.getInstance()
+
+        val spinnerID = findViewById<Spinner>(R.id.mySpinner)
+        val arrayAdapt = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, priorities)
+        spinnerID.adapter = arrayAdapt
+        val priority = false
+
+        spinnerID?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if (priorities[p2] == "false"){
+                    priority == false
+                }
+                else{
+                    priority == true
+                }
+                Toast.makeText(this@Project_Create, "item selected: ${priorities[p2]}" ,Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Toast.makeText(this@Project_Create, "item selected: Nothing" ,Toast.LENGTH_SHORT).show()
+            }
+        }
+
         projectReference = rootNode.getReference("projects/$currentUser")
     }
 
