def805a20ca4f087b3152ec92a2aa493dff0a2b1
Clene <104350902+Clene1@users.noreply.github.com>
2024-05-02 15:12:34 +0200

    Updates

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/ProjectsFragment.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/ProjectsFragment.kt
@@ -1,17 +1,25 @@
 package com.example.timeflow_opsc_poe_part_2
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class ProjectsFragment : Fragment() {
@@ -29,16 +37,49 @@ class ProjectsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //val btn = view.findViewById<Button>(R.id.btnproject)
+
         val currentUser = CurrentUser.userID
+        val btnCreateProject = view.findViewById<TextView>(R.id.btnAddProject)
+        listView = view.findViewById<ListView>(R.id.lvProjects)
         rootNode = FirebaseDatabase.getInstance()
         projectReference = rootNode.getReference("projects/$currentUser")
 
-        //logic for inputs
+        // setting up the list view
+        val context = context as MainActivity
+        val list = ArrayList<String>()
+        val IDList = ArrayList<String>()
 
-        //btn.setOnClickListener{
-        //    writeProject("Building house", true)
-        //}
+        val lv = context.findViewById(R.id.lvProjects) as ListView
+        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, list)
+        lv.adapter = adapter
+
+        // reading from dastabase
+        projectReference.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot){
+                IDList.clear()
+                for(snapshot1 in snapshot.children){
+                    val dc2 = snapshot1.getValue(Project::class.java)
+                    val txt = " ${dc2?.name}"
+                    IDList.add(snapshot1.key.toString())
+                    txt?.let {list.add(it)}
+                }
+                adapter.notifyDataSetChanged()
+            }
+            override fun onCancelled(error: DatabaseError){
+
+            }
+        })
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val element = parent.getItemAtPosition(position)
+            var id = element.toString().trim()
+            Toast.makeText(context, IDList[position], Toast.LENGTH_SHORT,).show()
+        }
+
+        btnCreateProject.setOnClickListener{
+            val intent = Intent(context, Project_Create::class.java)
+            startActivity(intent)
+        }
     }
 
 }
--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/ScheduleFragment.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/ScheduleFragment.kt
@@ -1,14 +1,25 @@
 package com.example.timeflow_opsc_poe_part_2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ScheduleFragment : Fragment() {
+    private  lateinit var listView : ListView
+    private  lateinit var rootNode : FirebaseDatabase
+    private  lateinit var projectReference : DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +32,47 @@ class ScheduleFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //open Select option
+        val currentUser = CurrentUser.userID
+        val btnCreateProject = view.findViewById<TextView>(R.id.btnAddEntry)
+        listView = view.findViewById<ListView>(R.id.lvProjects)
+        rootNode = FirebaseDatabase.getInstance()
+        projectReference = rootNode.getReference("projects/$currentUser")
 
+        // setting up the list view
+        val context = context as MainActivity
+        val list = ArrayList<String>()
+        val IDList = ArrayList<String>()
+
+        val lv = context.findViewById(R.id.lvProjects) as ListView
+        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, list)
+        lv.adapter = adapter
+
+        // reading from dastabase
+        projectReference.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot){
+                IDList.clear()
+                for(snapshot1 in snapshot.children){
+                    val dc2 = snapshot1.getValue(Project::class.java)
+                    val txt = " ${dc2?.name}"
+                    IDList.add(snapshot1.key.toString())
+                    txt?.let {list.add(it)}
+                }
+                adapter.notifyDataSetChanged()
+            }
+            override fun onCancelled(error: DatabaseError){
+
+            }
+        })
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val element = parent.getItemAtPosition(position)
+            var id = element.toString().trim()
+            Toast.makeText(context, IDList[position], Toast.LENGTH_SHORT,).show()
+        }
+
+        btnCreateProject.setOnClickListener{
+            val intent = Intent(context, Project_Create::class.java)
+            startActivity(intent)
+        }
     }
 }
