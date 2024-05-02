7a99f3ba296a40655a9d39c28dc8154989e94bad
ST10219213 <moolmans20013@gmail.com>
2024-05-02 15:03:32 +0200

    Browse and add photo

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/Manual_Entry.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/Manual_Entry.kt
@@ -1,17 +1,29 @@
 package com.example.timeflow_opsc_poe_part_2
 
 import android.os.Bundle
+import android.provider.OpenableColumns
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.play.integrity.internal.w
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+
 
 class Manual_Entry : AppCompatActivity() {
     private  lateinit var rootNode : FirebaseDatabase
     private  lateinit var timeEntriesReference : DatabaseReference
-
+    private  lateinit var imageView:ImageView
+    private val imageContract = registerForActivityResult(ActivityResultContracts.GetContent(),
+        ActivityResultCallback {
+            imageView.setImageURI(it)
+        })
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,11 +33,17 @@ class Manual_Entry : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // image stuff
+        val btnAddphoto = findViewById<Button>(R.id.btnAddPhoto)
+        imageView = findViewById(R.id.imgTimesheet)
+
+        btnAddphoto.setOnClickListener{
+            imageContract.launch("image/*")
+        }
+
         val currentUser = CurrentUser.userID
         rootNode = FirebaseDatabase.getInstance()
         timeEntriesReference = rootNode.getReference("timeEntries/$currentUser")
-
-
 
     }
 
@@ -36,5 +54,9 @@ class Manual_Entry : AppCompatActivity() {
         if (key != null) {
             timeEntriesReference.child(key).setValue(timeEntry)
         }
+    }
+
+    companion object{
+        val IMAGE_REQUEST_CODE = 100
     }
 }
--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/ScheduleFragment.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/ScheduleFragment.kt
@@ -1,10 +1,12 @@
 package com.example.timeflow_opsc_poe_part_2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -20,8 +22,12 @@ class ScheduleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val context = context as MainActivity
 
-        //open Select option
-
+        val btnAddEntry = view.findViewById<Button>(R.id.btnAddEntry)
+        btnAddEntry.setOnClickListener {
+            val intent = Intent(context, Select_Option::class.java)
+            startActivity(intent)
+        }
     }
 }
