b22071c65ec3b685ceb0281c4c915a6ba8190a10
ST10219213 <moolmans20013@gmail.com>
2024-05-03 13:59:30 +0200

    Finish timesheet upload

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/CurrentUser.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/CurrentUser.kt
@@ -1,5 +1,4 @@
 package com.example.timeflow_opsc_poe_part_2
-
 object CurrentUser {
     var userID = ""
 }
--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/Manual_Entry.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/Manual_Entry.kt
@@ -6,17 +6,28 @@ import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.storage
 import java.io.ByteArrayOutputStream
 
 
@@ -24,13 +35,15 @@ class Manual_Entry : AppCompatActivity() {
     private  lateinit var rootNode : FirebaseDatabase
     private  lateinit var timeEntriesReference : DatabaseReference
     private  lateinit var imageView:ImageView
+    private  lateinit var projectReference : DatabaseReference
     private  lateinit var storage:FirebaseStorage
+    var currentProject = ""
+    val currentUser = CurrentUser.userID
+    var photoRefernece = ""
 
     private val imageContract = registerForActivityResult(ActivityResultContracts.GetContent(),
         ActivityResultCallback {
             imageView.setImageURI(it)
-            val imageUri: Uri? = it
-
         })
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,47 +55,103 @@ class Manual_Entry : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // image stuff
+        // prior values
         val btnAddphoto = findViewById<Button>(R.id.btnAddPhoto)
         imageView = findViewById(R.id.imgTimesheet)
-        val imgnew = findViewById<ImageView>(R.id.imgTimesheet2)
+        rootNode = FirebaseDatabase.getInstance()
+        var projectsList:ArrayList<String> = populateProjects()
 
+        // populating projects list
+        populateProjects()
+
+        // populate dropdownList
+        populateDropdown(projectsList)
+
+        // adding photo to imageview
         btnAddphoto.setOnClickListener{
             imageContract.launch("image/*")
         }
 
-        val currentUser = CurrentUser.userID
-        rootNode = FirebaseDatabase.getInstance()
-        timeEntriesReference = rootNode.getReference("timeEntries/$currentUser")
 
         val btnSave = findViewById<Button>(R.id.btnSave)
         btnSave.setOnClickListener{
-            imgnew.setImageBitmap(byteArrayToBitmap(uploadImage()));
+            writeTimeEntry("Today", "Food", "1500", "1600")
+            upLoadImage(uploadToBytes())
         }
     }
 
-    fun uploadImage(): ByteArray{
+    fun uploadToBytes(): ByteArray{
         val bitmap = (imageView.getDrawable() as BitmapDrawable).bitmap
         val stream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
         return stream.toByteArray()
     }
 
+    fun upLoadImage(array:ByteArray){
+        storage = Firebase.storage
+        var storageRef = storage.reference
+        val mountainsRef = storageRef.child("images/$currentUser/$photoRefernece")
+        val data = array
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
+    }
+
+    fun populateDropdown(projectsList:ArrayList<String>){
+        val spinnerID = findViewById<Spinner>(R.id.mySpinnerProjects)
+        val arrayAdapt = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, projectsList)
+        spinnerID.adapter = arrayAdapt
+        spinnerID?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                currentProject = projectsList[p2]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+    }
+
+    fun populateProjects():ArrayList<String>{
+        var projectsList = ArrayList<String>()
+        projectReference = rootNode.getReference("projects/$currentUser")
+        projectReference.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot){
+                for(snapshot1 in snapshot.children){
+                    val dc2 = snapshot1.getValue(Project::class.java)
+                    if (dc2 != null) {
+                        projectsList.add(dc2.name)
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError){
+
+            }
+        })
+        return projectsList
+    }
+
+    // this method is used to reconvert to bitarray for imageview
     fun byteArrayToBitmap(data: ByteArray): Bitmap {
         return BitmapFactory.decodeByteArray(data, 0, data.size)
     }
 
-
-    fun writeTimeEntry(date: String, project: String, startTime: String, endTime: String, photoRefernece: String) {
+    fun writeTimeEntry(date: String, project: String, startTime: String, endTime: String) {
+        timeEntriesReference = rootNode.getReference("timeEntries/$currentUser/$date")
         var myRef = timeEntriesReference.push()
         var key = myRef.key
+        if (key != null) {
+            photoRefernece = key + "IMG"
+        }
         val timeEntry = TimesheetEntry( date, project, startTime, endTime, photoRefernece)
         if (key != null) {
             timeEntriesReference.child(key).setValue(timeEntry)
         }
     }
 
-    companion object{
-        val IMAGE_REQUEST_CODE = 100
-    }
 }
