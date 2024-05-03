d077144899392ddde7e5bc4e1e5a398baf391519
Clene <104350902+Clene1@users.noreply.github.com>
2024-05-03 18:59:24 +0200

    Set goals fixed

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/Set_One_Time_Goal.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/Set_One_Time_Goal.kt
@@ -3,7 +3,6 @@ package com.example.timeflow_opsc_poe_part_2
 import android.app.TimePickerDialog
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.TextView
 import android.widget.TimePicker
 import androidx.activity.enableEdgeToEdge
@@ -14,8 +13,7 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
-
-class Set_Daily_Goals : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
+class Set_One_Time_Goal : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
 
     private val calender = Calendar.getInstance()
     private val formatter = SimpleDateFormat("hh:mm a", Locale.UK)
@@ -23,14 +21,14 @@ class Set_Daily_Goals : AppCompatActivity(), TimePickerDialog.OnTimeSetListener 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_set_daily_goals)
+        setContentView(R.layout.activity_set_one_time_goal)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        findViewById<TextView>(R.id.txtMinDailyTime).setOnClickListener {
+        findViewById<TextView>(R.id.txtMinTimeOnce).setOnClickListener {
             displayFormattedTime1(calender.timeInMillis)
             TimePickerDialog(
                 this,
@@ -42,6 +40,7 @@ class Set_Daily_Goals : AppCompatActivity(), TimePickerDialog.OnTimeSetListener 
                         }
                         displayFormattedTime1(calender.timeInMillis)
                     }
+
                 },
                 calender.get(Calendar.HOUR_OF_DAY),
                 calender.get(Calendar.MINUTE),
@@ -49,7 +48,7 @@ class Set_Daily_Goals : AppCompatActivity(), TimePickerDialog.OnTimeSetListener 
             ).show()
         }
 
-        findViewById<TextView>(R.id.txtMaxDailyTime).setOnClickListener {
+        findViewById<TextView>(R.id.txtMaxTimeOnce).setOnClickListener {
             displayFormattedTime2(calender.timeInMillis)
             TimePickerDialog(
                 this,
@@ -61,6 +60,7 @@ class Set_Daily_Goals : AppCompatActivity(), TimePickerDialog.OnTimeSetListener 
                         }
                         displayFormattedTime2(calender.timeInMillis)
                     }
+
                 },
                 calender.get(Calendar.HOUR_OF_DAY),
                 calender.get(Calendar.MINUTE),
@@ -79,15 +79,12 @@ class Set_Daily_Goals : AppCompatActivity(), TimePickerDialog.OnTimeSetListener 
 
 
     fun displayFormattedTime1(timestamp: Long) {
-        findViewById<TextView>(R.id.txtMinDailyTime).text = formatter.format(timestamp)
+        findViewById<TextView>(R.id.txtMinTimeOnce).text = formatter.format(timestamp)
         Log.i("Formatting", timestamp.toString())
     }
 
     fun displayFormattedTime2(timestamp: Long) {
-        findViewById<TextView>(R.id.txtMaxDailyTime).text = formatter.format(timestamp)
+        findViewById<TextView>(R.id.txtMaxTimeOnce).text = formatter.format(timestamp)
         Log.i("Formatting", timestamp.toString())
     }
 }
-
-
-
