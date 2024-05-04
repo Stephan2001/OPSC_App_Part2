a7b060f2a3c5958c01f42ffb23d3c4dd78c1fe52
Clene <104350902+Clene1@users.noreply.github.com>
2024-05-04 13:51:28 +0200

    project drop down added

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/StatisticsFragment.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/StatisticsFragment.kt
@@ -5,8 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class StatisticsFragment : Fragment() {
+    private  lateinit var listView : ListView
+    private  lateinit var rootNode : FirebaseDatabase
+    private  lateinit var projectReference : DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,5 +26,45 @@ class StatisticsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_statistics, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val currentUser = CurrentUser.userID
+        listView = view.findViewById<ListView>(R.id.lvProjects)
+        rootNode = FirebaseDatabase.getInstance()
+        projectReference = rootNode.getReference("projects/$currentUser")
+
+        // setting up the list view
+        val context = context as MainActivity
+        val list = ArrayList<String>()
+        val IDList = ArrayList<String>()
+
+        val lv = context.findViewById(R.id.lvProjects) as ListView
+        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, list)
+        lv.adapter = adapter
+
+        // reading from dastabase
+        projectReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                IDList.clear()
+                for (snapshot1 in snapshot.children) {
+                    val dc2 = snapshot1.getValue(Project::class.java)
+                    val txt = " ${dc2?.name}"
+                    IDList.add(snapshot1.key.toString())
+                    txt?.let { list.add(it) }
+                }
+                adapter.notifyDataSetChanged()
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val element = parent.getItemAtPosition(position)
+            var id = element.toString().trim()
+            Toast.makeText(context, IDList[position], Toast.LENGTH_SHORT,).show()
+        }
     }
 }
