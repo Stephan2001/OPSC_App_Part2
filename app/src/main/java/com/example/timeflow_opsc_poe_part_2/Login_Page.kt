cd238ec628097f7eea80c34a6d1feca448f1b037
Clene <104350902+Clene1@users.noreply.github.com>
2024-05-04 22:57:24 +0200

    Login and register validation

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/Login_Page.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/Login_Page.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -11,17 +12,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.timeflow_opsc_poe_part_2.databinding.ActivityLoginPageBinding
+import com.example.timeflow_opsc_poe_part_2.databinding.ActivityRegistrationPageBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 
 class Login_Page : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         auth = Firebase.auth
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginPageBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_page)
+        setContentView(binding.root)
+        emailFocusListener()
+        passwordFocusListener()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -48,6 +56,56 @@ class Login_Page : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    //validate login
+    private fun emailFocusListener() {
+        binding.txtEmail.setOnFocusChangeListener{_, focused ->
+            if (!focused)
+            {
+                binding.emailContainer.helperText = validEmail()
+            }
+        }
+    }
+    //validate email
+    private fun validEmail(): String? {
+        val txtEmail = binding.txtEmail.text.toString()
+        if (!Patterns.EMAIL_ADDRESS.matcher(txtEmail).matches())
+        {
+            return "Invalid email address"
+        }
+        return null
+    }
+
+    //validate password
+    private fun passwordFocusListener() {
+        binding.txtPassword.setOnFocusChangeListener{_, focused ->
+            if (!focused)
+            {
+                binding.passwordContainer.helperText = validPassword()
+            }
+        }
+    }
+    //validate password
+    private fun validPassword(): String? {
+        val txtPassword = binding.txtPassword.text.toString()
+        if (txtPassword.length < 6)
+        {
+            return "Must be 6 characters long"
+        }
+        if (!txtPassword.matches(".*[A-Z].*".toRegex()))
+        {
+            return "Must contain at least 1 upper case character"
+        }
+        if (!txtPassword.matches(".*[a-z].*".toRegex()))
+        {
+            return "Must contain at least 1 lower case character"
+        }
+        if (!txtPassword.matches(".*[!@#$%&*.].*".toRegex()))
+        {
+            return "Must contain at least 1 special character (!@#$%&*.)"
+        }
+        return null
     }
 
     private fun signIn(email: String, password: String) {
