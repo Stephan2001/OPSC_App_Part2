d253432b127842bbb63ccb6dba81f89ff805c757
Clene <104350902+Clene1@users.noreply.github.com>
2024-05-05 16:42:29 +0200

    goalfragment change

--- app/src/main/java/com/example/timeflow_opsc_poe_part_2/GoalsFragment.kt
+++ app/src/main/java/com/example/timeflow_opsc_poe_part_2/GoalsFragment.kt
@@ -1,21 +1,13 @@
 package com.example.timeflow_opsc_poe_part_2
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.ListView
 import android.widget.TextView
-import android.widget.Toast
-import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -24,10 +16,6 @@ import com.google.firebase.database.ValueEventListener
 
 
 class GoalsFragment : Fragment() {
-    private  lateinit var listView : ListView
-    private  lateinit var rootNode : FirebaseDatabase
-    private  lateinit var goalsReference : DatabaseReference
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,10 +24,12 @@ class GoalsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_goals, container, false)
     }
 
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val context = context as MainActivity
+
+        var rootNode = FirebaseDatabase.getInstance()
+        var goalsReference = rootNode.getReference("goals/${CurrentUser.userID}")
 
         val setDailyGoal = view.findViewById<Button>(R.id.btnDailyGoal)
         setDailyGoal.setOnClickListener {
@@ -47,41 +37,17 @@ class GoalsFragment : Fragment() {
             startActivity(intent)
         }
 
-        val currentUser = CurrentUser.userID
-        val btnDailyGoal = view.findViewById<TextView>(R.id.btnDailyGoal)
-        listView = view.findViewById<ListView>(R.id.lvGoals)
-        rootNode = FirebaseDatabase.getInstance()
-        goalsReference = rootNode.getReference("goals/$currentUser")
-
-        // setting up the list view
-        val context = context as MainActivity
-        val list = ArrayList<String>()
-        val IDList = ArrayList<String>()
-
-        val lv = context.findViewById(R.id.lvGoals) as ListView
-        val adapter = ArrayAdapter(context, R.layout.goal_listitems, list)
-        lv.adapter = adapter
-
-        // reading from dastabase
         goalsReference.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot){
-                IDList.clear()
-                list.clear()
                 for(snapshot1 in snapshot.children){
                     val dc2 = snapshot1.getValue(DailyGoals::class.java)
-                    val txt = " ${dc2?.DailyMin}, ${dc2?.DailyMax}"
-                    txt?.let {list.add(it)}
+
                 }
-                adapter.notifyDataSetChanged()
             }
             override fun onCancelled(error: DatabaseError){
 
             }
         })
-
-
-
-
 
     }
 }
